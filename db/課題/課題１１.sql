8abbd83544eeeb046790a9aaa0a14f74e3e98ffe
namiki_miwa <namiki.miwa@aiskk.co.jp>
2021-08-31 17:13:28 +0900

    DB演習問題11の訂正

--- db/課題/課題１１.sql
+++ db/課題/課題１１.sql
@@ -1,4 +1,5 @@
-SELECT dep.dept_name, em.gender, COUNT(em.gender)
+SELECT 
+    dep.dept_name, em.gender, COUNT(gender) as gender_count
 FROM
     dept_emp AS depem
         LEFT OUTER JOIN
@@ -7,12 +8,18 @@ FROM
     employees AS em ON depem.emp_no = em.emp_no
         LEFT OUTER JOIN
     salaries AS sa ON sa.emp_no = em.emp_no
-where sa.salary >= (select AVG(sa.salary) 
-from     
-	dept_emp AS depem
-        LEFT OUTER JOIN
-    departments AS dep ON dep.dept_no = depem.dept_no
-        LEFT OUTER JOIN
-    salaries AS sa ON sa.emp_no = depem.emp_no
-   )
-    group by dep.dept_name, em.gender;
+WHERE
+    EXISTS( SELECT 
+            dep_avg.dept_name, AVG(sa_avg.salary) as avg_salary
+        FROM
+            dept_emp AS depem_avg
+                LEFT OUTER JOIN
+            departments AS dep_avg ON depem_avg.dept_no = dep_avg.dept_no
+                LEFT OUTER JOIN
+            employees AS em_avg ON depem_avg.emp_no = em_avg.emp_no
+                LEFT OUTER JOIN
+            salaries AS sa_avg ON em_avg.emp_no = sa_avg.emp_no
+        GROUP BY dep_avg.dept_name
+        HAVING dep_avg.dept_name = dep.dept_name
+            AND avg_salary <= sa.salary)
+GROUP BY dep.dept_name , em.gender;
